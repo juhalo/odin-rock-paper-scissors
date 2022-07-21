5a89354c6047c07efff7930d05da0b167c1d968d
juhalo <107361221+juhalo@users.noreply.github.com>
2022-07-21 20:09:24 +0300

    Add function to determine computer's choice
    The new function 'getComputerChoice()' chooses pseudo-randomly
    between 'Rock', 'Paper' and 'Scissors'. This is used by the
    computer opponent to determine their choice. The function works
    by using 'Math.random()' to choose a number between 0 and 3.
    It should be exclusive at 3 and inclusive at 0.
    After we have the number it's inputted into
    the Math.floor() function to force the number to be either 0, 1 or 2.
    
    There is a typo in a comment for the function and the odds are actually
    exactly equal to each other which contradicts what was said inside
    the function's comment. These will be fixed in the next commit.

--- external.js
+++ external.js
@@ -1,1 +1,28 @@
-console.log("Haist");
+
+function getComputerChoice () {
+    /* I know two functions Math.random() and Math.floor() and with
+    these two I believe I can force the number to be one of three values.
+    I chose these values to be 0,1,2 since starting from zero is natural
+
+    I have chosen to assign these the following way:
+    rock=0, paper=1 and scissors=2.
+
+    The way I am doing it might give an ever so slighly lesser chance
+    of getting scissors from the program but this made more sense
+    considering my current knowledge of the language (I know that
+    Google exists but I like my own solution more).
+    */
+    let choice = Math.floor(Math.random()*3);
+    /* I used switch since this kind of case where a value variable
+    can only be certain values feels more natural to do by using it
+    (at least with the knowledge I have gotten from the material I
+    have read so far). */
+    switch(choice) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
