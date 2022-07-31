9093f8bee92380a8bd2dcd7840a862fcb20b8034
juhalo <107361221+juhalo@users.noreply.github.com>
2022-07-31 14:14:05 +0300

    Create basic GUI for the game
    Makes buttons for rock, paper, and scissors. Changes the input method
    from prompts to the buttons for the user input. At this point the
    previous function 'game' is still included as a comment for future
    usage but will be removed in the future.

--- external.js
+++ external.js
@@ -1,58 +1,81 @@
-
-function getComputerChoice () {
-    // Forces the  values to be either 0 (rock), 1 (paper) or 2 (scissors)
-    let choice = Math.floor(Math.random()*3);
-    // Uses switch since only certain options (0,1,2) are possible
-    switch(choice) {
-        case 0:
-            return 'Rock';
-        case 1:
-            return 'Paper';
-        case 2:
-            return 'Scissors';
-    }
+function getComputerChoice() {
+  // Forces the  values to be either 0 (rock), 1 (paper) or 2 (scissors)
+  let choice = Math.floor(Math.random() * 3);
+  // Uses switch since only certain options (0,1,2) are possible
+  switch (choice) {
+    case 0:
+      return 'Rock';
+    case 1:
+      return 'Paper';
+    case 2:
+      return 'Scissors';
+  }
 }
 
-function playRound (playerSelection, computerSelection) {
-    // Converts playerSelection to same format as computerSelection
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
-    if (playerSelection === computerSelection) {
-        return `You Tied!`;
-    } else if ((playerSelection==='Scissors' && computerSelection==='Rock') ||
-            (playerSelection==='Rock' && computerSelection==='Paper') ||
-            (playerSelection==='Paper' && computerSelection==='Scissors')) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-
+function playRound(playerSelection, computerSelection) {
+  // Converts playerSelection to same format as computerSelection
+  playerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
+  if (playerSelection === computerSelection) {
+    return `You Tied!`;
+  } else if (
+    (playerSelection === 'Scissors' && computerSelection === 'Rock') ||
+    (playerSelection === 'Rock' && computerSelection === 'Paper') ||
+    (playerSelection === 'Paper' && computerSelection === 'Scissors')
+  ) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
 }
+
+function addEvent(clickedButton) {
+  let roundResult = playRound(clickedButton, getComputerChoice());
+  console.log(`Round ${round++ + 1}: ` + roundResult);
+  roundResult = roundResult.split(' ');
+  if (roundResult[1] === `Win!`) {
+    outcome += 1;
+  } else if (roundResult[1] === `Lose!`) {
+    outcome -= 1;
+  }
+}
+
+let outcome = 0;
+let round = 0;
+const buttons = document.querySelectorAll('button');
+let playerSelection = buttons.forEach((button) =>
+  button.addEventListener('click', () => addEvent(button.id))
+);
 
 //Starts the game
-function game () {
-    // Uses outcome variable to know if lost or won
-    let outcome = 0;
-    console.log("This is a game of \'Rock Paper Scissors\' versus a computer");
-    console.log("")
-    for (let i=0; i<5; i++) {
-        let playerSelection = prompt(`Type \'Rock\', \'Paper\' or \'Scissors\'`)
-        let roundResult = playRound(playerSelection, getComputerChoice());
-        console.log(`Round ${i+1}: ` + roundResult);
-        roundResult = roundResult.split(" ");
-        if (roundResult[1]===`Win!`) {
-            outcome += 1;
-        } else if (roundResult[1]===`Lose!`) {
-            outcome -= 1;
-        }
+/* function game() {
+  // Uses outcome variable to know if lost or won
+  let outcome = 0;
+  console.log("This is a game of 'Rock Paper Scissors' versus a computer");
+  console.log('');
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(`Type \'Rock\', \'Paper\' or \'Scissors\'`);
+    let roundResult = playRound(playerSelection, getComputerChoice());
+    console.log(`Round ${i + 1}: ` + roundResult);
+    roundResult = roundResult.split(' ');
+    if (roundResult[1] === `Win!`) {
+      outcome += 1;
+    } else if (roundResult[1] === `Lose!`) {
+      outcome -= 1;
     }
-    // Checks the outcome of the game
-    if (outcome===0) {
-        console.log(`\n`+`You tied the game.`);
-    } else if (outcome>0) {
-        console.log(`\n`+`Congratulations! You won the game!`);
-    } else {
-        console.log(`\n`+`You lost the game. Better luck next time.`);
-    }
+  }
+  // Checks the outcome of the game
+  if (outcome === 0) {
+    console.log(`\n` + `You tied the game.`);
+  } else if (outcome > 0) {
+    console.log(`\n` + `Congratulations! You won the game!`);
+  } else {
+    console.log(`\n` + `You lost the game. Better luck next time.`);
+  }
+  let newGame = prompt('Restart game: (Y)es, (N)o');
+  if (newGame === 'Y') {
+    game();
+  }
 }
 
-game()
+game(); */
